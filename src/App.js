a29d52c76e6348c3426ffc355c59195b0298a902
Mario <minaufmario.og@gmail.com>
2020-12-26 13:16:18 +0100

    Added my-carousal title-message scroll-down comp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 
 import './App.css';
 import MyNavbar from './components/my-navbar/mynavbar.component'
+import MyCarousal from './components/my-carousal/my-carousal.component'
+import TitleMessage from './components/title-message/title-message.component'
+
 
 const App = () => {
   return (
+    <>
     <MyNavbar/>
+    <MyCarousal/>
+    <TitleMessage/>
+    </>
   );
  
 }

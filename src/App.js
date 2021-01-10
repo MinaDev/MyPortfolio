aadb77d5482fbe8e066085e409f36c6846a97912
Mario <minaufmario.og@gmail.com>
2021-01-10 19:09:02 +0100

    Added experience page

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import MyNavbar from './components/my-navbar/mynavbar.component'
 import MyCarousal from './components/my-carousal/my-carousal.component'
 import TitleMessage from './components/title-message/title-message.component'
 import Fade from "react-reveal/Fade";
+import Slide from "react-reveal/Slide"
 import { Parallax } from "react-parallax";
 import About from "./pages/about/about.component";
 import Container from "react-bootstrap/Container";
 import BgImage from "./assets/img/parallex/background.webp"
 import Skills  from "./pages/skills/skills.component"
+import Experience from './pages/experience/experience.component'
 
 
 const App = () => {
@@ -34,12 +36,22 @@ const App = () => {
         </Parallax>
       </div>
       <div>
-            <Container className="container-box rounded">
-              <Fade duration={500}>
-      <Skills/>
-      </Fade>
-            </Container>
-            </div>
+        <Container className="container-box rounded">
+          <Slide bottom duration={500}>
+          <hr/>
+             <Skills/>
+          </Slide>
+        </Container>
+      </div>
+      <div>
+        <Container className="container-box rounded">
+          <Fade duration={500}>
+            <hr/>
+             <Experience/>
+          </Fade>
+        </Container>
+      </div>
+      
       
       </div>
   );
